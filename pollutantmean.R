7e4e04b6bd161d8566f07201b2bec34aaab1c3db
Patrick <phogan1@alumni.nd.edu>
2015-06-13 14:55:29 -0700

    Added input checks; began writing code to pull data

--- pollutantmean.R
+++ pollutantmean.R
@@ -14,8 +14,36 @@ pollutantmean <- function(directory, pollutant, id=1:332){
     ## NOTE: Do not round the result!
     
     ## START:
+    
+    ## Check Inputs: 
+    
     # Check that the directory exists: 
     stopifnot(length(directory)==1,dir.exists(directory))
     
+    # Check that the pollutant is a character vector containing exactly one of the allowed names:
+    allowedPollutants <- c("sulfate","nitrate");
+    stopifnot(is.character(pollutant),length(pollutant)==1,
+              grep(pollutant,allowedPollutants,ignore.case=TRUE)>0)
+    
+    # Check that the ID is an integer list:
+    stopifnot(is.integer(id) || (is.numeric(id) && identical(round(id),id)))
+    
+    # Remove bad values from the ID list: 
+    if ( any(id < 1) || any(id > 332) ){
+        warning("ID values out of range 1:332 detected and ignored.")
+    }
+    if ( any(is.na(id)) ) {
+        warning("NA ID values detected and ignored.")
+    }
+    id <- id[id > 0 & id < 333 & !is.na(id)]
+    stopifnot(length(id) > 0)
+    
+    ## Get the data: 
+    
+    for (n in id){
+        fname <- sprintf("%03d",n)
+        d = read.csv(file.path(directory,fname))
+        
+    }
     
 }
